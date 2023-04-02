2897a515402c69b84eebdbbf3f24a286d64bbed9
ryanchiang <ryanschiang@gmail.com>
2023-04-02 16:27:32 -0700

    add table support

--- src/exportMarkdown.js
+++ src/exportMarkdown.js
@@ -67,6 +67,60 @@
             markdown += `\`\`\`\n${text}\`\`\`\n`;
           }
 
+          // Tables
+          if (tag === "TABLE") {
+            // Get table sections
+            let tableMarkdown = "";
+            childNode.childNodes.forEach((tableSectionNode) => {
+
+              if (
+                tableSectionNode.nodeType === Node.ELEMENT_NODE &&
+                (tableSectionNode.tagName === "THEAD" ||
+                  tableSectionNode.tagName === "TBODY")
+              ) {
+                // Get table rows
+                let tableRows = "";
+                let tableColCount = 0;
+                tableSectionNode.childNodes.forEach(
+                  (tableRowNode) => {
+                    if (
+                      tableRowNode.nodeType === Node.ELEMENT_NODE &&
+                      tableRowNode.tagName === "TR"
+                    ) {
+                      // Get table cells
+                      let tableCells = "";
+                      
+                      tableRowNode.childNodes.forEach(
+                        (tableCellNode) => {
+                          if (
+                            tableCellNode.nodeType ===
+                              Node.ELEMENT_NODE &&
+                            (tableCellNode.tagName === "TD" ||
+                              tableCellNode.tagName === "TH")
+                          ) {
+                            tableCells += `| ${tableCellNode.textContent} `;
+                            if (tableSectionNode.tagName === "THEAD") {
+                              tableColCount++;
+                            }
+                          }
+                        }
+                      );
+                      tableRows += `${tableCells}|\n`;
+                    }
+                  }
+                );
+
+                tableMarkdown += tableRows;
+
+                if (tableSectionNode.tagName === "THEAD") {
+                  const headerRowDivider = `| ${Array(tableColCount).fill('---').join(' | ')} |\n`;
+                  tableMarkdown += headerRowDivider;
+                }
+              }
+            });
+            markdown += tableMarkdown;
+          }
+
           // Paragraph break after each element
           markdown += "\n";
         }
